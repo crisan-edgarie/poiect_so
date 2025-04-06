04f0fc8759deab0ea2be65c4044bca7001193d79
root <antoniu.andreas12@gmail.com>
2025-04-06 12:44:43 +0000

    gata frumos tot

--- treasure_manager.c
+++ treasure_manager.c
@@ -14,14 +14,14 @@ typedef struct{
   char id[4];
   char user_name[4];
   coord coordinates;
-  char clue[101];
+  char clue[100];
   unsigned int value;
 }treasure;
 
 void add_treasure(char *hunt_id)
 {
-  char hunt_path[6]="./";
-  char treasure_path[15]="./";
+  char hunt_path[50]="./";
+  char treasure_path[59]="./";
   struct stat file_desc_obj;
   int treasure_fd;
   treasure t;
@@ -46,7 +46,7 @@ void add_treasure(char *hunt_id)
   scanf("%f %f",&t.coordinates.latitude,&t.coordinates.longitude);
   while((fuck_c=getchar())!='\n') {}
   printf("Input treasure clue:");
-  fgets(t.clue,101,stdin);
+  fgets(t.clue,100,stdin);
   t.clue[strlen(t.clue)-1]='\0';
   printf("Input treasure value:");
   scanf("%u",&t.value);
@@ -69,7 +69,7 @@ void add_treasure(char *hunt_id)
 
 void list_hunt(char *hunt_id)
 {
-  char paths[15]="./";
+  char paths[59]="./";
   struct stat file_desc_obj;
 
   strcat(paths,hunt_id);
@@ -107,7 +107,7 @@ void list_hunt(char *hunt_id)
 
 void view_treasure(char *hunt_id,char *id){
 
-  char paths[15]="./";
+  char paths[59]="./";
   struct stat file_desc_obj;
 
   strcat(paths,hunt_id);
@@ -140,17 +140,103 @@ void view_treasure(char *hunt_id,char *id){
   close(treasure_fd);
 }
 
+void remove_hunt(char *hunt_id)
+{
+  char hunt_path[50]="./";
+  char treasure_path[59]="./";
+  struct stat file_desc_obj;
+
+  strcat(hunt_path,hunt_id);
+  strcat(treasure_path,hunt_id);
+  strcat(treasure_path,"/treasure");
+ 
+  if(stat(hunt_path,&file_desc_obj)==-1 && errno==ENOENT){
+  	printf("There is no such hunt.Hmmm...Must've been the wind\n");
+ 	return;
+  }
+
+  unlink(treasure_path);
+  unlinkat(AT_FDCWD,hunt_path,AT_REMOVEDIR);
+  
+}
+
+void remove_treasure(char *hunt_id,char *treasure_id)
+{
+  
+  char paths[59]="./";
+  struct stat file_desc_obj;
+
+  strcat(paths,hunt_id);
+  
+  if(stat(paths,&file_desc_obj)==-1 && errno==ENOENT){
+  	printf("There is no such hunt.Hmmm...Must've been the wind\n");
+ 	return;
+  }
+  strcat(paths,"/treasure");
+  if(stat(paths,&file_desc_obj)==-1 && errno==ENOENT){
+	printf("Oopsie daisy there is no treasure here.....damn.....so empty....\n");
+	return;
+  }
+  
+  treasure *wanted_treasure = (treasure *)malloc(file_desc_obj.st_size);
+
+  int fd = open(paths,O_RDONLY);
+  int treasure_index=0;
+
+  while(read(fd,wanted_treasure[treasure_index].id,4*sizeof(char)))
+  {
+	read(fd,wanted_treasure[treasure_index].user_name,4*sizeof(char));
+	read(fd,&wanted_treasure[treasure_index].coordinates.latitude,sizeof(float));
+	read(fd,&wanted_treasure[treasure_index].coordinates.longitude,sizeof(float));
+	read(fd,wanted_treasure[treasure_index].clue,100*sizeof(char));
+	read(fd,&wanted_treasure[treasure_index].value,sizeof(unsigned int));
+	treasure_index++;
+  }
+  
+  close(fd);
+  unlink(paths);
+  fd = open(paths,O_WRONLY | O_CREAT, 0777);
+
+  for(unsigned int i=0;i<treasure_index;i++)
+  {
+	if(strcmp(wanted_treasure[i].id,treasure_id)!=0){
+		write(fd,wanted_treasure[i].id,4*sizeof(char));
+		write(fd,wanted_treasure[i].user_name,4*sizeof(char));
+		write(fd,&wanted_treasure[i].coordinates.latitude,sizeof(float));
+		write(fd,&wanted_treasure[i].coordinates.longitude,sizeof(float));
+		write(fd,wanted_treasure[i].clue,100*sizeof(char));
+  		write(fd,&wanted_treasure[i].value,sizeof(unsigned int));
+	}
+  }
+  
+  close(fd);
+  free(wanted_treasure);
+}
+
+void help()
+{
+  printf("Usage:./treasure_manager --<op> <*args>\n\n");
+  printf("./treasure_manager --add <hunt_id>:{Adds one treasure to the selected hunt.If there is no such hunt one will be created}\n");
+  printf("./treasure_manager --list <hunt_id>:{Prints the id, size and last time of modification of the selected hunt, then prints all the treasure inside. If there is no such hunt or there is no treasure inside, an appropiate message will be displayed}\n");
+  printf("./treasure_manager --view_treasure <hunt_id> <treasure_id>:{Prints all of the treasures with the selected id from the selected hunt.  If there is no such hunt or there is no treasure inside, an appropiate message will be displayed}\n");
+  printf("./treasure_manager --remove_hunt <hunt_id>:{Removes the selected hunt if it exists}\n");
+  printf("./treasure_manager --remove_treasure <hunt_id> <treasure_id>:{Removes the selected treasure fromt the selected hunt.  If there is no such hunt or there is no treasure inside, an appropiate message will be displayed}\n}");
+  printf("----------------------------------------------------------------------------------------------------------------------------------------------------\n");
+}
+
 int main(int argc,char **argv)
 {
   if(argc == 1){
 	printf("Wrong usage of script.Usage:treasure_manager --<op> <*args>\nFor a more specific description of all operations use:treasure_manager --help\n" );
 	exit(-5);
   }
-  char *op = argv[1];
-  switch(op[2]){
-	case 'a':{ add_treasure(argv[2]); break;}
-	case 'l':{ list_hunt(argv[2]); break;}
-	case 'v':{ view_treasure(argv[2],argv[3]); break;}
- }
+  if(!strcmp(argv[1],"--add")){ add_treasure(argv[2]); return 0;}
+  if(!strcmp(argv[1],"--list")){ list_hunt(argv[2]); return 0;}
+  if(!strcmp(argv[1],"--view_treasure")){if(argc<4) printf("Insufficient number of arguments...\n"); else view_treasure(argv[2],argv[3]); return 0;}
+  if(!strcmp(argv[1],"--remove_hunt")){ remove_hunt(argv[2]); return 0;} 
+  if(!strcmp(argv[1],"--remove_treasure")){if(argc<4) printf("Insufficient number of arguments...\n"); else remove_treasure(argv[2],argv[3]); return 0;}
+  if(!strcmp(argv[1],"--help")) {help(); return 0;}
+ 
+  printf("No such command: %s\n",argv[1]);
   return 0;
 }
